4b614c8efae62bda545214b59196009e575ab9c8
happyu <290945266@qq.com>
2018-02-11 19:58:09 -0800

    使用react-loadable实现按需加载

--- todo-list/router/routers.js
+++ todo-list/router/routers.js
@@ -1,20 +1,22 @@
 import React from 'react'
-import Bundle from '../components/Bundle'
+import Loadable from 'react-loadable'
+// import Bundle from '../components/Bundle'
 import App from '../containers/App'
+import Loading from '../components/Loading'
 // import Test from '../components/Test'
 
 
-const Test = (props) => (
-    <Bundle load={() => import('../components/Test')}>
-        { (Test) => <Test {...props} /> }
-    </Bundle>
-)
 
-const Home = (props) => (
-    <Bundle load={() => import('../components/Home')}>
-        { (Home) => <Home {...props} /> }
-    </Bundle>
-)
+const AsyncTest = Loadable({
+    loader: () => import('../components/Test'),
+    loading: Loading
+})
+
+const AsyncHome = Loadable({
+    loader: () => import('../components/Home'),
+    loading: Loading
+})
+
 
 const routes = [
     {
@@ -24,12 +26,21 @@ const routes = [
     },
     {
         path: '/index',
-        component: Test
+        component: AsyncTest
     },
     {
         path: '/home',
-        component: Home
+        component: AsyncHome
+    },
+    {
+        path: 'empty',
+        component: null
     }
 ]
+
+// 说明
+// empty Route
+// https://github.com/ReactTraining/react-router/issues/1982  解决人：PFight
+// 解决react-router v4改变查询参数并不会刷新或者说重载组件的问题
 
 export default routes
